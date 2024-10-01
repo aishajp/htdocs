2b779d38c5d550be2a196ec6f6a858d62fdbb88a
aishajp <aishajp09@educastur.es>
2024-10-01 10:17:58 +0200

    Entrono_servidor_Yolanda

---
+++ Pruebas/Ejercicios_Pruebas/Ejercicio1.php
@@ -0,0 +1,18 @@
+<?php
+
+$num1=20;
+$num2=20;
+echo "<Los numeros son: ".$num1." y ".$num2."<br>";
+if($num1 == $num2){
+    echo "Los numeros son iguales";
+    $solucion = $num1 * $num2;
+    echo "<br>La multiplicacion es: ".$solucion;
+}else if ($num1 > $num2){
+    echo "El numero mayor es: ".$num1;
+    $solucion = $num1 - $num2;
+    echo "<br>La resta es: ".$solucion;
+}else{
+    echo "El numero mayor es: ".$num2;
+    $solucion = $num2 + $num1;
+    echo "<br>La suma es: ".$solucion;
+}
