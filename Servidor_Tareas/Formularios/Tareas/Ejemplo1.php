321e974f9d81c867322b543a5ce3297436fcca93
aishajp <aishajp09@educastur.es>
2024-10-21 13:32:30 +0200

    Servidor

---
+++ Servidor_Tareas/Formularios/Tareas/Ejemplo1.php
@@ -0,0 +1,14 @@
+<?php
+echo"<form>";
+for ($i = 0; $i <10; $i++) {
+    echo<<<_END
+        <label for="$i">$i:</label>
+        <input type="text" id="num1" name="$i" required>
+        <br>
+        _END;
+}
+echo<<<_END
+<input type="submit" value="Enviar">
+_END;
+echo "</form>";
+?>
