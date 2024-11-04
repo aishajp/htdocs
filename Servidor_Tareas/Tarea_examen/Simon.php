7a4ddb961ce2adcd936bb299ab775018fb1b1017
aishajp <aishajp09@educastur.es>
2024-11-04 14:03:38 +0100

    Servidor

---
+++ Servidor_Tareas/Tarea_examen/Simon.php
@@ -0,0 +1,7 @@
+<?php
+/* Generar una combinacion de 4 colores, y un boton de jugar.
+Pantalla principal 4 circulos negros con 4 botones con 1 color 
+diferente cada uno y luego comprobar si la combinacion que yo 
+puse es igual a la q se mostro anteriormente y comprobarlo */
+
+?>
