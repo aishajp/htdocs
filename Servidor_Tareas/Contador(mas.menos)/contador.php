9bedaf0a409fb65f9696e84dca095c8bd31dd710
aishajp <aishajp09@educastur.es>
2024-11-11 13:15:09 +0100

    Servidor

---
+++ Servidor_Tareas/Contador(mas.menos)/contador.php
@@ -0,0 +1,18 @@
+<?php
+    session_start();
+
+?>
+
+<!DOCTYPE html>
+<html lang="esp">
+<head>
+    <meta charset="UTF-8">
+    <title>Contador</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+        }
+        </style>
+</head>
+
+</html>
