e55d4b95ef3e92669cc982d588301f35a513ea59
aishajp <aishajp09@educastur.es>
2024-11-11 13:26:15 +0100

    Servidor

--- Servidor_Tareas/Contador(mas.menos)/contador.php
+++ Servidor_Tareas/Contador(mas.menos)/contador.php
@@ -1,5 +1,20 @@
+
 <?php
-    session_start();
+
+    session_start();       
+        if(isset($_POST['contador'])) {
+            $_SESSION['contador'] = $_POST['contador'];
+        }
+
+        if(isset($_POST['contador']) && $_POST['contador'] > 0) {
+            $_SESSION['contador']--;
+        } elseif(isset($_POST['contador']) && $_POST['contador'] < 0) {
+            $_SESSION['contador']++;
+        }
+        
+        if(!isset($_SESSION['contador'])) {
+            $_SESSION['contador'] = 0;
+        }
 
 ?>
 
@@ -8,11 +23,16 @@
 <head>
     <meta charset="UTF-8">
     <title>Contador</title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-        }
-        </style>
 </head>
+<body>
+    <h1>Contador</h1>
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+        <input type="hidden" name="contador" value="<?php echo $_SESSION['contador'];?>">
+        <input type="submit" value="-">
+        <input type="submit" value="<?php echo $_SESSION['contador'];?>">
+        <input type="submit" value="+">
+    </form>
+    <p>Valor actual: <?php echo $_SESSION['contador'];?></p>
+</body>
 
 </html>
